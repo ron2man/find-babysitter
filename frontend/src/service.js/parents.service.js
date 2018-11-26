47ff81021139a914369524f785ccd645e9c9f3e8
Ido <idokestin@gmail.com>
2018-11-26 18:29:14 +0000

    fron service and data edit

---
+++ frontend/src/service.js/parents.service.js
@@ -0,0 +1,73 @@
+
+var parents = [
+    {
+        id:'makeId()',
+        name: 'אתי',
+        parentAge:39,
+        children:[
+            {
+                name:'יובל',
+                age:3,
+            },
+            {
+                name:'עומר',
+                age:3,
+            },
+
+
+        ],
+        phone: '0522345334',
+        parentGender: 'אמא',
+        languages:['עברית'],
+        descritption:'יובלי ועומר שלנו ילדים מקסימים, אוהבים לשחק ומאוד נוחים.',
+        shortCall: true,
+        joinedAt: '20.05.2017',
+        lastVisit: 'לפני שבוע',
+        imgUrl: '',
+    },
+    {
+        id:'makeId()',
+        name: 'שובל',
+        parentAge:36,
+        children:[
+            {
+                name:'מיקי',
+                age:2,
+            }
+        ],
+        phone: '0544789300',
+        parentGender: 'אמא',
+        languages:['עברית'],
+        descritption:'מיקי ילד מהמם ברמות, לא בגלל שאני אמא שלו:)',
+        shortCall: true,
+        joinedAt: '20.05.2017',
+        lastVisit: 'לפני שבוע',
+        imgUrl: '',
+    },
+    {
+        id:'makeId()',
+        name: 'טלי',
+        age: 3,
+        children:[
+            {
+                name:'מיקי',
+                age:2,
+            }
+        ],
+        phone: '0544789300',
+        parentGender: 'אמא',
+        parentAge:36,
+        children:[
+            {
+                name:'מיקי',
+                age:2,
+            }
+        ],
+        languages:['עברית'],
+        descritption:'יוני ילד מהמם ברמות, לא בגלל שאני אמא שלו:)',
+        shortCall: true,
+        joinedAt: '20.05.2017',
+        lastVisit: 'לפני שבוע',
+        imgUrl: '',
+    },
+]
