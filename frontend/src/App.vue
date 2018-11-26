8603867bf25d332ff570450ba78e7ebc5f12ef6f
ron2man Ron Shtaiman <ron2man10@gmail.com>
2018-11-26 18:29:17 +0200

    third

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div id="app">
+  <header>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+  </header>
+    <main>
     <router-view/>
+</main>
+    <footer></footer>
   </div>
 </template>
 
