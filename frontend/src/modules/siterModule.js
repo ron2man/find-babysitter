ca56d630e1f869e16d7c13f3b3642cc523d8a2d3
thelastpako <44051881+thelastpako@users.noreply.github.com>
2018-11-27 20:09:20 +0200

    fix css

--- frontend/src/modules/siterModule.js
+++ frontend/src/modules/siterModule.js
@@ -6,19 +6,11 @@ export default {
         currentSiter: {},
     },
     mutations: {
-<<<<<<< HEAD
-        setsiters(state, siters) {
-            state.currentSiter = siters
-        },
-        setCurrentSiter(state, siter) {
-            state.siter = siter
-=======
         setsiters(state,{siters}){
             state.siters = siters
         },
         setCurrentSiter(state,siter){
             state.currentSiter = siter
->>>>>>> f6176e92c866bfd00f8d19e82d89f5302b0a4b56
         },
         removeSiter(state, siterIdx) {
             state.siters.splice(siterIdx, 1)
@@ -31,17 +23,11 @@ export default {
     actions: {
         getsitersList(context) {
             return siterService.query()
-<<<<<<< HEAD
-                .then(siters => context.commit('setsiters', siters))
-        },
-        getById(context, id) {
-=======
                 .then(siters => {
                     context.commit({type:'setsiters',siters})
             })
         },
         getById(context,id){
->>>>>>> f6176e92c866bfd00f8d19e82d89f5302b0a4b56
             return siterService.getById(id)
                 .then(siter => context.commit('setCurrentSiter', siter))
         },
@@ -51,17 +37,12 @@ export default {
         },
         updateSiter(context, siter) {
             return siterService.updateSiter(siter)
-<<<<<<< HEAD
-                .then(siter => context.commit('updateSiter', siter))
-        }
-=======
                 .then(siter => context.commit('updateSiter',siter))
         },
-        setFilter(filter){
+        setFilter(context,filter){
             siterService.query(filter)
               .then(siters => context.commit('setsiters',siters))
                 },    
->>>>>>> f6176e92c866bfd00f8d19e82d89f5302b0a4b56
     },
     getters: {
         getSiters: (state) => { return state.siters },
