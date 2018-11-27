291591626b3cb3ff9c5456ecd09303a724d45e88
Ido <idokestin@gmail.com>
2018-11-27 11:58:32 +0000

    data

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
+import Sign from './views/Sign.vue'
 
 Vue.use(Router)
 
@@ -18,6 +19,11 @@ export default new Router({
       path: '/login',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/sign',
+      name: 'sign',
+      component: Sign
     },
     {
       path: '/about',
---
+++ frontend/src/views/Sign.vue
@@ -0,0 +1,65 @@
+
+
+
+<template>
+  <section class="login-container flex column">
+    <h1 class="login-title">Sign up</h1>
+    <form @submit.prevent="setUser" class="flex column">
+      <input type="text" placeholder="Choose nick name">
+      <input type="text" placeholder="Insert password">
+      <input type="text" placeholder="Insert password again">
+      <input type="text" placeholder="Insert password again">
+      <input type="text" placeholder="Insert password again">
+      <input type="text" placeholder="Insert password again">
+      <input type="text" placeholder="Insert password again">
+      <span v-if="isWrong">worng credinatls</span>
+      <button>Submit</button>
+      <p class="sign-in">not yet registered?
+        <router-link to="/sign" class="pwd-forgot">sign in</router-link>
+      </p>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "SignPage",
+  data() {
+    return {
+      newUser: {
+        id:'makeId()',
+        nickName:'',
+        pwd:'',
+        name: '',
+        children:[
+            {
+                name:'',
+                age:'',
+            },
+        ],
+        phone: '',
+        adrress:{
+            district:'',
+            city:'',
+        },
+        parentGender: '',
+        languages:[],
+        descritption:'',
+        shortCall: true,
+        joinedAt: 'moment()',
+        lastVisit: '',
+        imgUrl: '',
+      },
+      isWrong: flase
+    };
+  },
+  methods: {
+    setUser() {
+      this.$store.dispatch({ type: "setUser", user: this.newUser });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
