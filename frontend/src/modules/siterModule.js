2a818bdf5f3d004a740879475bbfed8b90c8bf21
thelastpako <44051881+thelastpako@users.noreply.github.com>
2018-11-27 19:51:57 +0200

    ffs

--- frontend/src/modules/siterModule.js
+++ frontend/src/modules/siterModule.js
@@ -2,46 +2,45 @@ import siterService from '../services/siterService'
 
 export default {
     state: {
-        siters:[],
-        currentSiter:{},
+        siters: [],
+        currentSiter: {},
     },
     mutations: {
-        setsiters(state,siters){
+        setsiters(state, siters) {
             state.currentSiter = siters
         },
-        setCurrentSiter(state,siter){
+        setCurrentSiter(state, siter) {
             state.siter = siter
         },
-        removeSiter(state, siterIdx){
-            state.siters.splice(siterIdx,1)
+        removeSiter(state, siterIdx) {
+            state.siters.splice(siterIdx, 1)
         },
-        updateSiter(state,siter){
+        updateSiter(state, siter) {
             const siterIdx = state.siters.findIndex(currSiter => currSiter.id === siter.id)
-            state.siters.splice(siterIdx,1,1)
+            state.siters.splice(siterIdx, 1, 1)
         }
     },
     actions: {
-        getsitersList(context){
+        getsitersList(context) {
             return siterService.query()
-                .then(siters => context.commit('setsiters',siters))
-            },
-        getById(context,id){
+                .then(siters => context.commit('setsiters', siters))
+        },
+        getById(context, id) {
             return siterService.getById(id)
-                .then(siter => context.commit('setCurrentSiter',siter))
+                .then(siter => context.commit('setCurrentSiter', siter))
         },
-        removeSiter(context,id){
+        removeSiter(context, id) {
             return siterService.removeSiter(id)
-            .then(siterIdx => context.commit('removeSiter',siterIdx))
+                .then(siterIdx => context.commit('removeSiter', siterIdx))
         },
-        updateSiter(context,siter){
+        updateSiter(context, siter) {
             return siterService.updateSiter(siter)
-                .then(siter => context.commit('updateSiter',siter))
+                .then(siter => context.commit('updateSiter', siter))
         }
     },
     getters: {
-        getSiters: (state) => {return state.siters},
-        getCurrentSiter: (state) => {return state.currentSiter},
+        getSiters: (state) => { return state.siters },
+        getCurrentSiter: (state) => { return state.currentSiter },
     }
 }
-  
-  
+
