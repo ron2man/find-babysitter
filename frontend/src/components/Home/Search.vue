77901bf8243d13278f8df1a3f55f2fcd769d24ec
ron2man Ron Shtaiman <ron2man10@gmail.com>
2018-11-28 18:47:59 +0200

    comit

--- frontend/src/components/Home/Search.vue
+++ frontend/src/components/Home/Search.vue
@@ -1,57 +1,57 @@
 <template>
-  <form @submit.prevent="setFilter">
-    <div class="search">
-      <!-- ADRESS INPUT START -->
-      <div class="inputs adress">
-        <label for="text">&#32;</label>
-        <input
-          ref="autocomplete"
-          :placeholder="filter.location.address"
-          class="s1earch-location"
-          onfocus="value = ''"
-          type="text"
-          
-        >
+  <!-- <form @submit.prevent="setFilter"> -->
+  <div class="search-container">
+    <!-- ADRESS INPUT START -->
+    <div class="inputs row-container adress">
+      <!-- <label for="text">&#32;</label> -->
+      <input
+        ref="autocomplete"
+        :placeholder="filter.location.address"
+        class="s1earch-location"
+        onfocus="value = ''"
+        type="text"
+      >
+    </div>
+    <!-- ADRESS INPUT END -->
+    <!-- DATE AND TIME INPUTS STARTS-->
+    <div class="date-and-time row-container flex flex-space-evenly">
+      <!-- INPUT DATE START -->
+      <div class="inputs date">
+        <date-pick v-model="filter.date" input-class="test" :displayFormat="'DD.MM.YYYY'"></date-pick>
       </div>
-      <!-- ADRESS INPUT END -->
-      <!-- DATE AND TIME INPUTS -->
-      <div class="date-and-time flex">
-        <!-- INPUT DATE START -->
-        <div class="inputs date">
-          <label for="date"></label>
-          <date-pick v-model="filter.date" input-class="test" :displayFormat="'DD.MM.YYYY'"></date-pick>
-        </div>
-        <!-- INPUT DATE END -->
-        <div class="inputs time-start">
-          <label for="time-start">From</label>
-          <vue-timepicker
-            format="HH:mm"
-            :minute-interval="30"
-            hide-clear-button
-            v-model="filter.startTime"
-          ></vue-timepicker>
-        </div>
-        <div class="inputs time-end">
-          <label for="time-end">Till</label>
-          <vue-timepicker
-            format="HH:mm"
-            :minute-interval="30"
-            hide-clear-button
-            v-model="filter.endTime"
-          ></vue-timepicker>
-        </div>
+      <!-- INPUT DATE END -->
+      <div class="inputs time-start">
+        <vue-timepicker
+          format="HH:mm"
+          :minute-interval="30"
+          hide-clear-button
+          v-model="filter.startTime"
+        ></vue-timepicker>
       </div>
-      <div class="inputs button">
-        <label for="button"></label>
-        <button>Search</button>
+      <div class="inputs time-end">
+        <vue-timepicker
+          format="HH:mm"
+          :minute-interval="30"
+          hide-clear-button
+          v-model="filter.endTime"
+        ></vue-timepicker>
       </div>
     </div>
-  </form>
+    <!-- DATE AND TIME INPUTS STARTS-->
+    <!-- BUTTON INPUTS STARTS-->
+    <div class="inputs button">
+      <button>Search</button>
+    </div>
+    <!-- BUTTON INPUTS END-->
+  </div>
+  <!-- </form> -->
 </template>
 
 <script>
-import VueTimepicker from "vue2-timepicker";
-import DatePick from "vue-date-pick";
+// import VueTimepicker from "vue2-timepicker";
+import VueTimepicker from "./Form/vue-timepicker.vue";
+import DatePick from "./Form/vueDatePick.vue";
+// import DatePick from "vue-date-pick";
 
 export default {
   components: {
@@ -77,9 +77,8 @@ export default {
   },
   methods: {
     setFilter() {
-      
       this.$store.dispatch("setFilter", this.filter);
-      this.$router.push({ path: '/baby/list' })
+      this.$router.push({ path: "/baby/list" });
     }
   },
   created() {
@@ -102,87 +101,113 @@ export default {
       this.filter.location.lat = lat;
       this.filter.location.lon = lon;
       this.filter.location.address = this.$refs.autocomplete.value;
-      
-      // this.filter.location.address = `${ac[1].long_name} ${ac[0].long_name} ${
-      //   ac[2].long_name
-      // } ${ac[4].long_name}`;
-      // console.log(
-      //   `The user picked ${city} with the coordinates ${lat}, ${lon}`
-      //   , ac
-      // );
     });
   }
 };
 </script>
 
 <style lang="scss" scoped>
-* > input {
-  text-align: center;
-  font-size: 1.2em;
-  height: 30px;
-  padding: 0;
-  margin: 0;
-  border: 0;
-  outline: none;
-}
-.search {
-  background-color: white;
-  input,
-  input:focus {
-    border: 0;
-    outline: none;
+.search-container {
+  background: white;
+  border: 1px blue solid;
+  width: 90%;
+  padding: 0 10px;
+  margin: 0 auto;
+
+  .inputs {
+    border: 3px black solid;
+    input {
+      width: 100%;
+      border: 0;
+      outline: none;
+      padding: 10px 0;
+      text-align: center;
+      font-size: 1.2em;
+    }
   }
 }
-
-.search-location {
-  display: block;
-  width: 60vw;
-  margin: 0 auto;
-  margin-top: 5vw;
-  font-size: 20px;
-  font-weight: 400;
-  outline: none;
-  height: 30px;
-  line-height: 30px;
-  text-align: center;
-  border-radius: 10px;
-}
-form {
-  width: 90%;
-  margin: 0 auto;
-  padding-bottom: 15px;
-}
-.inputs {
-  width: 100%;
-  margin-bottom: 10px;
-
-  // height: 50px;
-}
-.inputs > * {
-  // display: block;
-  width: 100%;
-}
-input[type="search"] {
-  height: 32px;
-}
-
-label {
-  height: 16px;
-  text-align: initial;
-  padding-left: 10px;
-}
-button {
-  height: 32px;
-}
-
 @media (min-width: 767px) {
-  .search {
+  .search-container {
     display: flex;
     justify-content: space-evenly;
-    align-items: center;
-  }
-  .button {
-    width: 30%;
+    input {
+      display: inline-block;
+    }
+    .date-and-time .inputs {
+      width: 100%;
+    }
   }
 }
+// }
+
+// * > input {
+//   text-align: center;
+//   font-size: 1.2em;
+//   height: 30px;
+//   padding: 0;
+//   margin: 0;
+//   border: 0;
+//   outline: none;
+// }
+
+// .search {
+//   background-color: white;
+//   input,
+//   input:focus {
+//     border: 0;
+//     outline: none;
+//   }
+// }
+
+// .search-location {
+//   display: block;
+//   width: 60vw;
+//   margin: 0 auto;
+//   margin-top: 5vw;
+//   font-size: 20px;
+//   font-weight: 400;
+//   outline: none;
+//   height: 30px;
+//   line-height: 30px;
+//   text-align: center;
+//   border-radius: 10px;
+// }
+// form {
+//   width: 90%;
+//   margin: 0 auto;
+//   padding-bottom: 15px;
+// }
+// .inputs {
+//   width: 100%;
+//   margin-bottom: 10px;
+
+//   // height: 50px;
+// }
+// .inputs > * {
+//   // display: block;
+//   width: 100%;
+// }
+// input[type="search"] {
+//   height: 32px;
+// }
+
+// label {
+//   height: 16px;
+//   text-align: initial;
+//   padding-left: 10px;
+// }
+// button {
+//   height: 32px;
+// }
+
+// @media (min-width: 767px) {
+//   .search {
+//     display: flex;
+//     justify-content: space-evenly;
+//     align-items: center;
+//   }
+//   .button {
+//     width: 30%;
+//   }
+// }
 </style>
