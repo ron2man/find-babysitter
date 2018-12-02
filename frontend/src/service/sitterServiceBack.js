63af208d966c13749abc466f4ecc0d68cf99c551
Ido <idokestin@gmail.com>
2018-12-02 21:21:55 +0000

    merge

--- frontend/src/service/sitterServiceBack.js
+++ frontend/src/service/sitterServiceBack.js
@@ -4,8 +4,6 @@ const BASE_URL = 'http://localhost:3003'
 
 
 
-
-
 function query(filterLocation = '', filterProperty = '') {
     // function query(filter = {username:'shira',license: true}) {
 
@@ -47,6 +45,7 @@ var endTime = 1546336800000;
 
 
 
+
 function getById(id){
     return axios.get(`${BASE_URL}/baby/${id}`).then(res => res.data)
 }
@@ -58,6 +57,7 @@ function getByUsername(username){
 
 function updateUser(user){
     return axios.put(`${BASE_URL}/baby/${user._id}`,user)
+}
 
 
 function getSitterByUsername(username){
