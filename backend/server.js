9a114e271f8f383495ba42404256cb469afc5934
thelastpako <44051881+thelastpako@users.noreply.github.com>
2018-12-05 20:39:16 +0200

    dlskd

--- backend/server.js
+++ backend/server.js
@@ -6,16 +6,13 @@ const session = require('express-session')
 const app = express()
 const http = require('http').Server(app);
 var io = require('socket.io')(http, { origins: 'http://localhost:8080' });
-const history = require('connect-history-api-fallback');
 
 
 // const userService = require('./services/user-service')
 
 const addSitterRoutes = require('./routes/sitterRoute')
 const addUserRoutes = require('./routes/userRoute')
-const mongoService = require('./services/mongo.service')
 
-var historymsgs = {}
 
 app.use(cors({
   origin: ['http://localhost:8080'],
@@ -35,7 +32,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.use(history())
 app.use(express.static('public'));
 
 addSitterRoutes(app)
