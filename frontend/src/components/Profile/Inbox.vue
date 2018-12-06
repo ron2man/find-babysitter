81f421aeb870c888c335e27dabd41a37fe29136d
ron2man Ron Shtaiman <ron2man10@gmail.com>
2018-12-06 14:55:32 +0200

    fix-profile

---
+++ frontend/src/components/Profile/Inbox.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="inbox">
+    <!-- TODO:
+    1. CHANGE - isRead to isUnread-->
+    <!-- START MESSAGE -->
+    <div
+      @click="goToChat(notification.from)"
+      class="message flex align-items-center"
+      :class="{ active: notification.isRead }"
+      v-for="(notification, index) in getNotifications"
+      :key="index"
+    >
+      <div class="profile-image"></div>
+      <div class="msg-text">
+        <!-- {{notification}} -->
+        <h3 class="sender-name">{{notification.from}}</h3>
+        <!-- <h3 class="text-preview">this is my message to you</h3> -->
+        <h4 class="time">{{notification.createdAt | relativeTime}}</h4>
+      </div>
+      <div class="icon">
+        <!-- <i class="far fa-comment-alt"></i> -->
+        <i class="fas fa-circle"></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // props: ['notifications'],
+  computed: {
+    getNotifications() {
+      return this.$store.getters.getCurrentProfile.notifications;
+      // return this.$store.getters.getCurrentProfile;
+    }
+  },
+  methods: {
+    goToChat(from) {
+      this.$store.dispatch({ type: "changeNotificationStatus", from });
+      this.$router.push(`/baby/profile/sitter/${from}/contact`);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.profile-image {
+  width: 60px;
+  height: 60px;
+  background-color: #ccc;
+  background-image: url(https://ffx-small-sites.s3.amazonaws.com/findababysitter/production/ProfileImages/9062d7166ec64150bfcb0a8fe4addb13.jpg);
+  background-position: center center;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  border-radius: 50%;
+  align-self: center;
+  margin: 0 15px;
+}
+
+.profile-image.main {
+  width: 85px;
+  height: 85px;
+}
+.inbox .message {
+  text-align: left;
+  padding: 10px 0;
+  margin-top: 5px;
+  border-bottom: 1px solid black;
+}
+
+.message .msg-text > * {
+  padding: 5px 0;
+}
+
+.message .msg-text .time {
+  font-size: 0.8em;
+}
+
+.message .icon {
+  margin: 0 10px;
+}
+
+.active {
+  font-weight: bold;
+  text-transform: capitalize;
+}
+</style>
---
+++ frontend/src/components/Profile/Schedule.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="schedule">
+    <div class="schedule-item" v-for="(item, idx) in getSchedules" :key="idx">
+        {{item}} 
+      <div class="date-time flex align-items-center flex-space-evenly">
+        <div class="date">
+          <i class="fas fa-calendar-alt" style="padding:0 10px"></i>
+          <span>{{item.start | getDate}}</span>
+        </div>
+        <div class="start-time">14:00</div>
+        <p>-</p>
+        <div class="end-time">20:00</div>
+      </div>
+      <div class="address">Habonim 4 Ramat Gan</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+      computed: {
+    getSchedules() {
+      return this.$store.getters.getCurrentProfile.schedule;
+      // return this.$store.getters.getCurrentProfile;
+    }
+  },
+  filters: {
+      getDate(timestamp){
+          var time = new Date(timestamp)
+          return `${time.getDay()}/${time.getMonth()}/${time.getFullYear()}`
+      }
+  }
+};
+</script>
+
+<style>
+.schedule .schedule-item {
+  padding: 10px 0;
+  font-size: 1.3em;
+  border-bottom: 1px solid black;
+}
+
+.schedule-item .address {
+  padding: 10px 0;
+  text-align: center;
+}
+</style>
