7fdb9b84ca2374c2d2d84c9e42eb2ec86b97efcd
thelastpako <pakokap1990@gmail.com>
2018-12-06 12:37:44 +0200

    yes

--- frontend/src/components/dateTime.vue
+++ frontend/src/components/dateTime.vue
@@ -1,31 +1,37 @@
 <template>
   <section class="date-time">
-    <datepicker :inline="true" v-model="date" :format="customFormatter" :highlighted="highlighted"></datepicker>
-    <div class="time-container">
-      <div class="inputs time-start">
-        <p class="time-head">Start Time:</p>
-        <vue-timepicker
-          format="HH:mm"
-          :minute-interval="30"
-          hide-clear-button
-          v-model="startTime"
-          @change="getTimestampStart(startTime)"
-        ></vue-timepicker>
+    <div class="date-time-container">
+      <datepicker
+        :inline="true"
+        v-model="date"
+        :format="customFormatter"
+        :highlighted="highlighted"
+      ></datepicker>
+      <div class="time-container">
+        <div class="inputs time-start">
+          <p class="time-head">Start Time:</p>
+          <vue-timepicker
+            format="HH:mm"
+            :minute-interval="30"
+            hide-clear-button
+            v-model="startTime"
+            @change="getTimestampStart(startTime)"
+          ></vue-timepicker>
+        </div>
+        <div class="inputs time-end">
+          <p class="time-head">End Time:</p>
+          <vue-timepicker
+            format="HH:mm"
+            :minute-interval="30"
+            hide-clear-button
+            v-model="endTime"
+            @change="getTimestampEnd(endTime)"
+          ></vue-timepicker>
+        </div>
+        <button @click="book(sitter)">Book Now!</button>
       </div>
-      <div class="inputs time-end">
-        <p class="time-head">End Time:</p>
-        <vue-timepicker
-          format="HH:mm"
-          :minute-interval="30"
-          hide-clear-button
-          v-model="endTime"
-          @change="getTimestampEnd(endTime)"
-        ></vue-timepicker>
-      </div>
-      <button @click="book(sitter)">Book Now!</button>
     </div>
-    <!-- timstamp start:{{this.startTimestamp}}
-    timestamp end:{{this.endTimestamp}}-->
+    <div class="book-answer">{{bookMsg}}</div>
   </section>
 </template>
 
@@ -43,8 +49,10 @@ export default {
   methods: {
     book(sitter) {
       decodeURI;
-      if (!this.startTimestamp || !this.endTimestamp)
-        console.log("feel the fields");
+      if (!this.startTimestamp || !this.endTimestamp || !this.date) {
+        this.bookMsg = "Feel All The fields please!";
+        return;
+      }
       const reservation = {
         start: this.startTimestamp,
         end: this.endTimestamp,
@@ -57,10 +65,10 @@ export default {
       this.$store
         .dispatch({ type: "checkAvalability", reservation })
         .then(res => {
-          console.log(res)
-          if (res.length !== 0) console.log("sry already Taken babe");
+          if (res.length !== 0)
+            this.bookMsg = "Sorry, this date already booked";
           else {
-            console.log('res sentttt')
+            this.bookMsg = "Invatation sent, waiting for reply";
             this.$store.dispatch({ type: "sendRequest", reservation, sitter });
           }
         });
@@ -78,12 +86,14 @@ export default {
       this.date = moment(date).format("YYYY-MM-D");
     },
     getTimestampStart(time) {
+      this.bookMsg = "";
       if (this.date)
         this.startTimestamp = moment(
           `${this.date},${time.HH}:${time.mm}`
         )._d.getTime();
     },
     getTimestampEnd(time, data) {
+      this.bookMsg = "";
       if (this.date)
         this.endTimestamp = moment(
           `${this.date},${time.HH}:${time.mm}`
@@ -114,7 +124,9 @@ export default {
         dates: [
           // Disable an array of dates
         ]
-      }
+      },
+      sent: false,
+      bookMsg: ""
     };
   }
 };
@@ -124,7 +136,7 @@ export default {
 .time-container {
   margin-left: 30px;
 }
-.date-time {
+.date-time-container {
   display: flex;
   margin: auto;
   text-align: center;
